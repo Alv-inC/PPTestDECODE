922659e1bcd0d09876d3f765f521a63a19d53ad7
Alvin <alvinc1005@gmail.com>
2025-09-21 14:41:48 -0700

    added a test autonomous w/ finite state machine structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/autoTest.java
@@ -0,0 +1,114 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+//Visualizer: https://visualizer.pedropathing.com/
+@Autonomous(name = "autoTest", group = "Tests")
+public class autoTest extends OpMode {
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+    private int pathState;
+
+    //MAYBE LATER PUT ALL THE POSES INSIDE A INITIALIZATION FUNCTION
+    private final Pose startPose = new Pose(62,20, Math.toRadians(120));
+    private PathChain firstArtPos;
+    private PathChain grabArtLine;
+    private PathChain firstStartPos;
+
+
+    //DEFINE THE PATHS --> REPLACE POSE WITH FINAL POS LATER
+    public void buildPaths() {
+        firstArtPos = follower.pathBuilder()
+                .addPath(
+                        new BezierCurve(
+                                new Pose(62.000, 20.000),
+                                new Pose(63.499, 36.666),
+                                new Pose(36.666, 35.437)
+                        )
+                )
+                .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(180))
+                .build();
+
+        grabArtLine = follower.pathBuilder()
+                .addPath(new BezierLine(
+                        new Pose(36.666, 35.437),
+                        new Pose(15.158, 35.642)
+                ))
+                .setTangentHeadingInterpolation()
+                .build();
+
+        /* This is line3. Another BezierLine, but reversed. */
+        firstStartPos = follower.pathBuilder()
+                .addPath(new BezierLine(
+                        new Pose(15.158, 35.642),
+                        new Pose(61.656, 19.664)
+                ))
+                .setTangentHeadingInterpolation()
+                .setReversed()
+                .build();
+    }
+
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.holdPoint(follower.getPose());
+                follower.followPath(firstArtPos);
+                setPathState(1);
+                break;
+            case 1:
+            /* You could check for
+            - Follower State: "if(!follower.isBusy()) {}"
+            - Time: "if(pathTimer.getElapsedTimeSeconds() > 1) {}"
+            - Robot Position: "if(follower.getPose().getX() > 36) {}"
+            */
+                if (!follower.isBusy()) {
+
+                    follower.followPath(grabArtLine, true);
+                    setPathState(2);
+                }
+                break;
+            case 2:
+                if (!follower.isBusy()) {
+                    follower.followPath(firstStartPos, true);
+                    setPathState(3);
+                }
+                break;
+
+        }}
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void init() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+        follower = Constants.createFollower(hardwareMap);
+        buildPaths();
+        follower.setStartingPose(startPose);
+    }
+
+    @Override
+    public void loop() {
+
+        follower.update();
+        autonomousPathUpdate();
+
+        //Feedback to Driver Hub for debugging
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", follower.getPose().getHeading());
+        telemetry.update();
+    }
+}
