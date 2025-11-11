e14190d2a7e19531ffef85af5263c1515e29f479
Alvin <alvinc1005@gmail.com>
2025-11-10 18:29:14 -0800

    I dont even know

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/subSystemTuning/motorTEST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/subSystemTuning/motorTEST.java
@@ -1,31 +1,28 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.subSystemTuning;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
+@Configurable
 @TeleOp(name = "areWeCooked")
 public class motorTEST extends OpMode {
 
-    private DcMotorEx shooterL;
-            private DcMotorEx shooterR;
+            private Servo test;
+            public static double testVal;
     @Override
     public void init() {
-        shooterL = hardwareMap.get(DcMotorEx.class, "shooterLeft");
-        shooterR = hardwareMap.get(DcMotorEx.class, "shooterRight");
-        shooterL.setDirection(DcMotorSimple.Direction.REVERSE);
+        test = hardwareMap.get(Servo.class, "testMotor");
 
     }
 
     @Override
     public void loop() {
-        if(gamepad1.a){
-            shooterL.setPower(1);
+        test.setPosition(testVal);
 
-        }
-        if(gamepad1.b){
-            shooterR.setPower(1);
-        }
+
     }
 }
