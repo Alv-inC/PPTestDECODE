363df43ada2e88086dd8727505e1526056b246a5
Alvin <alvinc1005@gmail.com>
2026-01-14 21:04:27 -0800

    12 balls auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Subsystems/flyWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Subsystems/flyWheel.java
@@ -97,7 +97,7 @@ public class flyWheel {
  //
     public void constantShootAuto(){
         p = 0.15;
-        targetVelocity = -1200;
+        targetVelocity = -1140;
     }
         public void constantShoot(){
             p = 0.15;
@@ -130,7 +130,7 @@ public class flyWheel {
         //targetVelocity = 0;
     }
     public boolean isCurrentVelocityEnough(){
-        return fly1.getVelocity() <= -1200;
+        return fly1.getVelocity() <= -1140;
     }
 
         public boolean atSpeed() {
