760e6dce1e227b49e97715f2b93322cebe9b61f6
Alvin <alvinc1005@gmail.com>
2026-01-28 19:29:16 -0800

    18 ball

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Subsystems/flyWheel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Subsystems/flyWheel.java
@@ -101,7 +101,7 @@ public class flyWheel {
     }
     public void constantShootAutoSlow(){
         p = 0.15;
-        targetVelocity = -1050;
+        targetVelocity = -1250;
     }
     public void constantShootAutoFast(){
         p = 0.15;
