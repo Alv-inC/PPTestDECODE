6b3554d444f567257218c42df005f906e536ec6a
Alvin <alvinc1005@gmail.com>
2026-02-02 18:04:25 -0800

    18 ball

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/subSystemTuning/NewTurretTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/subSystemTuning/NewTurretTuning.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.pedroPathing.subSystemTuning;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -10,8 +13,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Subsystems.LimelightCamera;
 import org.firstinspires.ftc.teamcode.pedroPathing.Subsystems.TurretPLUSIntake;
+import org.firstinspires.ftc.teamcode.pedroPathing.Subsystems.flyWheel;
 
-@Config
+@Configurable
 @TeleOp(name = "Turret Tuning [NEW]", group = "Tuning")
 public class NewTurretTuning extends OpMode {
 
@@ -22,6 +26,8 @@ public class NewTurretTuning extends OpMode {
     public static int targetTagId = 20;
     public static boolean trackingEnabled = false;
     private DcMotorEx intake;
+    private final TelemetryManager panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+
 
     @Override
     public void init() {
@@ -36,6 +42,7 @@ public class NewTurretTuning extends OpMode {
         intake = hardwareMap.get(DcMotorEx.class, "intake");
         turret = new TurretPLUSIntake(hardwareMap, telemetry, intake);
         limelightCamera = new LimelightCamera(hardwareMap, telemetry);
+
         telemetry.addLine("Turret Tuning Ready");
         telemetry.addData("Initial Position", turret.getCurrentPosition());
         telemetry.update();
@@ -56,11 +63,14 @@ public class NewTurretTuning extends OpMode {
         turret.setTargetPosition(targetPosition);
         turret.update();
 
-        telemetry.addData("Position", turret.getCurrentPosition());
-        telemetry.addData("Target Position", TurretPLUSIntake.targetPosition);
-        telemetry.addData("Power", turret.getPow());
-        telemetry.addData("Time", timer.seconds());
-        telemetry.update();
+        updateSignals();
+    }
+
+    private void updateSignals() {
+        double t = timer.seconds();
+        panelsTelemetry.addData("current speed", turret.getCurrentPosition());
+        panelsTelemetry.addData("goal speed", targetPosition);
+        panelsTelemetry.update();
     }
 }
 
