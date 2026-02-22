eb6f18f5da0c520e3f6268bd23c3448fe9a33cfb
Alvin <alvinc1005@gmail.com>
2026-02-21 16:31:40 -0800

    retuned constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Autonomous/blueAutov3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Autonomous/blueAutov3.java
@@ -33,7 +33,7 @@ public class blueAutov3 extends OpMode {
     private int pathState;
     private TelemetryManager telemetryM;
     //MAYBE LATER PUT ALL THE POSES INSIDE A INITIALIZATION FUNCTION
-    private final Pose startPose = new Pose(19.0325, 132.5, Math.toRadians(135));
+    private final Pose startPose = new Pose(18.0325, 133, Math.toRadians(135));
 
     private PathChain firstLine;
     private PathChain Switch;
@@ -73,9 +73,9 @@ public class blueAutov3 extends OpMode {
         // === SHOTS PATHS ===
         Shot1 = follower.pathBuilder().addPath(
                         new BezierLine(
-                                new Pose(19.0325, 132.5),
+                                new Pose(18.0325, 133),
 
-                                new Pose(56.410, 78.455)
+                                new Pose(56.5, 78.455)
                         )
                 ).setLinearHeadingInterpolation(Math.toRadians(135), Math.toRadians(180))
 
@@ -92,7 +92,7 @@ public class blueAutov3 extends OpMode {
         Shot3 = follower.pathBuilder()
                 .addPath(new BezierLine(
                         new Pose(0, 68),
-                        new Pose(58, 72.455)
+                        new Pose(58.2, 72.455)
                 ))
                 .setConstantHeadingInterpolation(Math.toRadians(180))
                 .build();
