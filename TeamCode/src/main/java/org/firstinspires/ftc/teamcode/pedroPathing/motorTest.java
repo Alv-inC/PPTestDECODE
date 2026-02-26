2451d5e38cc5c080d7ad5dc714774ba9d9dc87e8
Alvin <alvinc1005@gmail.com>
2026-02-25 18:25:26 -0800

    retuned constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/motorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/motorTest.java
@@ -5,41 +5,24 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name = "DualMotorControl")
+@TeleOp(name = "MotorControl")
 @Config
 public class motorTest extends OpMode {
 
-    private DcMotor fly1, fly2, intake;
-    public static double motorPower = 0.5; // Adjustable from FTC Dashboard
-
+    private DcMotorEx fly1;
     @Override
     public void init() {
-        fly1 = hardwareMap.get(DcMotor.class, "fly1");
-        fly2 = hardwareMap.get(DcMotor.class, "fly2");
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        telemetry = FtcDashboard.getInstance().getTelemetry();
-        telemetry.addData("Status", "Initialized");
+        fly1 = hardwareMap.get(DcMotorEx.class, "motor");
+
     }
 
     @Override
     public void loop() {
         if (gamepad1.a) {
-            fly1.setPower(Math.abs(motorPower));
-            fly2.setPower(Math.abs(motorPower));
-            intake.setPower(Math.abs(motorPower));
+            fly1.setPower(0.3);
 
-
-        } else if (gamepad1.y) {
-            fly2.setPower(-Math.abs(motorPower));
-            fly2.setPower(Math.abs(motorPower));
-            intake.setPower(-Math.abs(motorPower));
-        } else if (gamepad1.b) {
-            fly1.setPower(0);
-            fly2.setPower(0);
-            intake.setPower(0);
         }
-
-        telemetry.update();
     }
 }
